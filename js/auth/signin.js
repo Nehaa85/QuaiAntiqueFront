7217c29962f64e72f8e8731cb706dc33a67d2631
Nehaa85 <schumi.dono@gmail.com>
2024-01-31 12:55:03 +0100

    Masquer éléments par role + auth dans le routage

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -12,6 +12,7 @@ function checkCredientals(){
         setToken(token);
         //placer ce token en cookie
 
+        setCookie(RoleCookieName, "client", 7);
         window.location.replace("/");
     }
     else{
